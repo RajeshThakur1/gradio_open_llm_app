14fdbf468447e321c734018596bf5024953e43a0
Rajesh123222 <Rajesh.Kumar3@synechron.com>
2024-06-22 13:01:11 +0530

    feat created the gradio app

---
+++ gradio_app.py
@@ -0,0 +1,47 @@
+import gradio as gr
+import requests
+
+def generate_response(context, user_question, model):
+    url = "http://localhost:8000/api/generate"
+    data = {"context": context, "user_question": user_question, "model": model}
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        return response.json()['response'], ""
+    return "Failed to connect to the server", ""
+
+def submit_rating(response_text, rating):
+    if rating is not None and response_text != "":
+        url = "http://localhost:8000/api/save_rating"
+        data = {"response": response_text, "rating": rating}
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            return "Rating submitted successfully!"
+        return f"Failed to submit rating: {response.status_code}"
+    return "No rating submitted"
+
+
+with gr.Blocks() as iface:
+    with gr.Row():
+        context_input = gr.Textbox(label="Context")
+        question_input = gr.Textbox(label="User Question")
+        model_input = gr.Dropdown(label="Model", choices=["llama3:latest", "llama2:latest", "mistral:latest", "phi3:latest","gemma:latest"])
+        submit_btn = gr.Button("Generate Response")
+
+    response_output = gr.Textbox(label="Response", interactive=False)
+    rating_input = gr.Slider(label="Rate the response", minimum=1, maximum=5, step=1)
+    rating_btn = gr.Button("Submit Rating")
+    rating_output = gr.Label()
+
+    submit_btn.click(
+        generate_response,
+        inputs=[context_input, question_input, model_input],
+        outputs=[response_output, rating_input]
+    )
+
+    rating_btn.click(
+        submit_rating,
+        inputs=[response_output, rating_input],
+        outputs=rating_output
+    )
+
+iface.launch()
